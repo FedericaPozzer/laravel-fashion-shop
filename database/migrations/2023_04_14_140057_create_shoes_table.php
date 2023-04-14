262f7a1f0083900728beb29f0c9c378c478e0b8a
Federica Pozzer <federica.pozzer@gmail.com>
2023-04-14 16:44:20 +0200

    filled table

--- database/migrations/2023_04_14_140057_create_shoes_table.php
+++ database/migrations/2023_04_14_140057_create_shoes_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('shoes', function (Blueprint $table) {
             $table->id();
+            $table->string("marca", 50);
+            $table->string("modello", 50);
+            $table->float("prezzo", 8, 2); //total digits - decimali
+            $table->string("descrizione", 300);
+            $table->string("immagine", 50);
             $table->timestamps();
         });
     }
