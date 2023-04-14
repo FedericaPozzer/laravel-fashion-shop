8c8cfcede56171b78c3be51cb42a5181e962af41
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-04-14 16:17:36 +0200

    userseeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = "Dominik";
+        $user->mail = "dom@gmail.com";
+        $user->password = "TORETTO";
+        $user->save();
+    }
+}
