e93b8462a83ab30ec8172dbf88a22bc87e475cd2
Federica Pozzer <federica.pozzer@gmail.com>
2023-04-17 16:20:38 +0200

    partials (modal e navbar)

--- resources/views/shoes/show.blade.php
+++ resources/views/shoes/show.blade.php
@@ -28,33 +28,4 @@
   </div>
 @endsection
 
-@section('modals')
-
-        <div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5 text-danger" id="exampleModalLabel">ATTENZIONE!</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                Sei sicuro di voler eliminare {{$shoe->modello}}? <br>
-                Questa operazione non è reversibile.
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-
-                <form action="{{route('shoes.destroy', $shoe)}}" method="POST">
-                  @method('delete')
-                  @csrf
-
-                  <button type="submit" class="btn btn-primary">Conferma</button>
-
-                </form>
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-@endsection
+@include("partials.modal")
